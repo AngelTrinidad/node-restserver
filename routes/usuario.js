dcb96fc7cc2da0015dbaef6c9636ea85e881ec54
AngelTrinidad <96trinidad@gmail.com>
2018-08-25 09:11:55 -0400

    configuracion de servidor

---
+++ routes/usuario.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const Usuario = require('../models/usuario');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+
+const app = express();
+
+app.get('/usuario', function (req, res) {
+
+  let desde = Number(req.query.desde) || 0;
+  let limite = Number(req.query.limite) || 5;
+
+  //Primer parametro, filtros, segundo, campos a mostrar
+  Usuario.find({estado:true}, 'nombre email role estado google img')
+    .skip(desde)
+    .limit(limite)
+    .exec((err, usuarios) => {
+
+      if(err){
+        return res.status(400).json({
+          ok: false,
+          status: 400,
+          err
+        });
+      }
+
+      //Count: mismos filtros del find
+      Usuario.count({estado:true}, (err, conteo) => {
+        res.json({
+          ok: true,
+          usuarios,
+          total: conteo
+        });
+      });
+    });
+});
+
+app.post('/usuario', function (req, res) {
+
+  let body = req.body;
+
+  //Schema
+  let usuario = new Usuario({
+    nombre: body.nombre,
+    email: body.email,
+    password: bcrypt.hashSync(body.password, 10),
+    role: body.role
+  });
+
+  //Save
+  usuario.save((err, usuarioDB) => {
+    if(err){
+      return res.status(400).json({
+        ok: false,
+        status: 400,
+        err
+      });
+    }
+
+    res.json({
+      persona: usuarioDB
+    });
+
+  });
+
+});
+
+app.put('/usuario/:id', function (req, res) {
+
+  let id = req.params.id;
+  //filtrado de campos permitidos con el underscore
+  let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+  Usuario.findByIdAndUpdate(id, body, {new: true, runValidators: true},(err, usuarioDB) => {
+    if(err){
+      return res.status(400).json({
+        ok: false,
+        err
+      });
+    }
+    res.json({
+      ok: true,
+      usuario: usuarioDB
+    });
+  });
+
+});
+
+app.delete('/usuario/:id', function (req, res) {
+
+  let id = req.params.id;
+
+  //Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+  let cambiaEstado = {estado:false}
+  Usuario.findByIdAndUpdate(id, cambiaEstado, {new:true},(err, usuarioBorrado) => {
+    if(err){
+      return res.status(400).json({
+        ok: false,
+        err
+      });
+    }
+
+    if(!usuarioBorrado){
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: 'Usuario no encontrado'
+        }
+      });
+    }
+
+    res.json({
+      ok: true,
+      deleted: usuarioBorrado
+    });
+  });
+});
+
+module.exports = app;
--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,14 @@
 //Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//Base de datos
+let urlDB;
+//if(process.env.NODE_ENV === 'dev'){
+//  urlDB = 'mongodb://localhost:27017/cafe';
+//}else{
+  urlDB = 'mongodb://cafe_user:Angelt1!@ds133202.mlab.com:33202/cafe-node-angeltrinidad';
+//}
+process.env.URLDB = urlDB;
